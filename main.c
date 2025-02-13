154070c1c842c2945f82c2afe81425970aae3cf4
Samuele-Scandellari <samuscan01@gmail.com>
2025-02-13 14:26:19 +0100

    Esercizio 5.18

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int array[10];
+    srand(time(0));
+    for (int i = 0; i < 10; i++) {
+        array[i] = 1 + rand() % 100;
+    }
+    printf("Array dall'inizio alla fine:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    printf("Array dalla fine all'inizio:\n");
+    for (int i = 9; i >= 0; i--) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    return 0;
+}
